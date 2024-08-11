30a8029e7e25c836f257ea58bcc338054f06e515
alokshandilya <alok.shandilya10@gmail.com>
2024-08-11 20:37:07 +0530

    chore: Add CRUD functionality for posts in FastAPI app
    - use Python List as fake db
    - basic pydantic model for validation
    - fastapi practices with dealing delete request
    - add CRUD functionality (update later)

--- app.py
+++ app.py
@@ -1,7 +1,65 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Response, status
+from pydantic import BaseModel
+from random import randrange
+
 
 app = FastAPI()
 
+
+class Post(BaseModel):
+    title: str
+    content: str
+
+
+my_posts = [
+    {"title": "Post 1", "content": "This is the content of post 1", "id": 1},
+    {"title": "Post 2", "content": "This is the content of post 2", "id": 2},
+]
+
+
+def find_posts(id):
+    for post in my_posts:
+        if post["id"] == id:
+            return post
+    return None
+
+
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"data": "Hello World"}
+
+
+@app.get("/posts")
+def read_posts():
+    return {"data": my_posts}
+
+
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def create_post(post: Post):
+    post_dict = post.model_dump()
+    post_dict["id"] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+    return {"data": "Post created successfully"}
+
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    post = find_posts(id)
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"post with id: {id} was not found",
+        )
+    return {"post_detail": post}
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    post = find_posts(id)
+    if post == None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"post with id: {id} was not found",
+        )
+    my_posts.remove(post)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
