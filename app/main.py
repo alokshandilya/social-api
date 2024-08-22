766d39b1e599300a849548252e77ba4b52a00dea
alokshandilya <alok.shandilya10@gmail.com>
2024-08-22 23:37:44 +0530

    feat: add password hashing
    - installed passlib and bcrypt via `pip install "passlib[bcrypt]"`
    - added `utils.py` containing the `hash` function
    - updated `main.py` to hash the password before storing it in the
      database
    - added a new endpoint to get a user by id

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from fastapi import Depends, FastAPI, HTTPException, Response, status
 from sqlalchemy.orm import Session
 
-from . import models, schemas
+from . import models, schemas, utils
 from .database import engine, get_db
 
 load_dotenv()
@@ -89,8 +89,23 @@ def update_post(
     response_model=schemas.UserOut,
 )
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    # hash the password - user.password
+    hashed_password = utils.hash(user.password)
+    user.password = hashed_password
+
     new_user = models.User(**user.model_dump())
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user
+
+
+@app.get("/users/{id}", response_model=schemas.UserOut)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"user with id: {id} was not found",
+        )
+    return user
---
+++ app/utils.py
@@ -0,0 +1,7 @@
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def hash(plain_password: str):
+    return pwd_context.hash(plain_password)
