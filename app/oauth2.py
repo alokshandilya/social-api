3b0ab86b0b13835e8685bb64cace898256b493b1
alokshandilya <alok.shandilya10@gmail.com>
2024-08-26 05:42:46 +0530

    chore: add oauth2 and jwt token generation
    - pip install "pyjwt[crypto]"
    - add oauth2.py to handle jwt token generation
    - use fastapi.security.OAuth2PasswordRequestForm to get username and
      password from the request as per OAuth2 specification
      - OAth2 specification dictates that the specific fields `username` and
        `password` should be used to get the user credentials as form data
        instead of JSON.
    - TODO: verify token, check token expiration

---
+++ app/oauth2.py
@@ -0,0 +1,22 @@
+import os
+from datetime import datetime, timedelta, timezone
+
+import jwt
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECTRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(to_encode, SECTRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,28 +1,23 @@
 from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm.session import Session
 
-from app import models, schemas, utils
+from app import models, oauth2, utils
 from app.database import get_db
 
 router = APIRouter(tags=["Authentication"])
 
 
 @router.post("/login")
-def login(user_credentials: schemas.UserLogin, db: Session = Depends(get_db)):
-    user = (
-        db.query(models.User)
-        .filter(models.User.email == user_credentials.email)
-        .first()
-    )
-    if not user:
+def login(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db),
+):
+    user = db.query(models.User).filter(models.User.email == form_data.username).first()
+    if not user or not utils.verify(form_data.password, user.password):
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Credentials"
         )
-    if not utils.verify(user_credentials.password, user.password):
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Credentials"
-        )
-    # create a token
 
-    # return a token
-    return {"message": "example token"}
+    access_token = oauth2.create_access_token(data={"user_id": user.id})
+    return {"access_token": access_token, "token_type": "bearer"}
