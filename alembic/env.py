1e9fa01b77825183487cf1d8e76429cc4c226d7e
alokshandilya <alok.shandilya10@gmail.com>
2024-08-31 22:17:13 +0530

    chore: update alembic env.py and models.py
    Update alembic env.py to use the database credentials from the settings
    file.
    
    - set the target_metadata to the Base.metadata in the models.py
    file, so that the alembic can generate the migration files.

--- alembic/env.py
+++ alembic/env.py
@@ -3,10 +3,16 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config, pool
 
 from alembic import context
+from app.config import settings
+from app.models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option(
+    "sqlalchemy.url",
+    f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}",
+)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -17,7 +23,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
