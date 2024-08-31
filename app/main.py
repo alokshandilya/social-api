3d967c8288454e826237b58d56d5c975bf1d920f
alokshandilya <alok.shandilya10@gmail.com>
2024-09-01 02:19:11 +0530

    chores: deploy to vercel, add CORS middleware
    - add vercel.json for vercel deployment
    - add CORS middleware to allow all origins

--- app/main.py
+++ app/main.py
@@ -1,8 +1,29 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers import auth, post, user, vote
 
 app = FastAPI()
+
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
